cd67a475436256225c198b3f04ad4f195cf282db
Bowen Zhang <bowenzhang@eduroam-int-dhcp-97-87-166.ucl.ac.uk>
2018-11-07 15:04:33 +0000

    Updated on 11th Nov.

--- Rcode.R
+++ Rcode.R
@@ -16,16 +16,14 @@ NHSData <- data.frame(NHSData)
 NHSMapSF <- read_shape("England_nhsat_2013/england_nhsat_2013.shp", as.sf = TRUE)
 NHSMapSP <- as(NHSMapSF, "Spatial")
 NHSDataMap <- append_data(NHSMapSF,NHSData, key.shp = "code", key.data = "New.code", ignore.duplicates = TRUE)
-qtm(NHSDataMap, fill = "One.year.net.survival.for.woman.in.1997")
-NHS_osm <- read_osm(NHSDataMap, type = "esri", zoom = NULL)
-qtm(NHS_osm) + 
+tmap_mode("view")
+qtm(NHSDataMap)+
   tm_shape(NHSDataMap) + 
   tm_polygons("One.year.net.survival.for.woman.in.1997", 
               style="jenks",
               palette="YlOrBr",
               midpoint=NA,
-              title="Net survival percentage",
+              title="Rate for survival",
               alpha = 0.5) + 
-  tm_compass(position = c("left", "bottom"),type = "arrow") + 
   tm_scale_bar(position = c("left", "bottom")) +
-  tm_layout(title = "One-year net survival for woman dignosed oesophageal cancer in 1997", legend.position = c("right", "bottom"))
+  tm_layout(title = "One year net survival for woman diagnosed esophagus cancer in 1997")
